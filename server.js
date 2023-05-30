fa13858fcae79492fc2bbb2ce3762dfdc4942f40
vhernandez1 <ing.victorhhernandez@gmail.com>
2023-05-30 12:08:59 -0500

    heroku fix app name

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/test-angular-banco-union'));
+app.use(express.static(__dirname + '/dist/banco-union'));
 
 app.get('/*', function (req, res) {
 
-  res.sendFile(path.join(__dirname + '/dist/test-angular-banco-union/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/banco-union/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
