8c7ee3186afcf32f6748427b8da7a9c6cea4c654
vhernandez1 <ing.victorhhernandez@gmail.com>
2023-05-30 15:20:27 -0500

    fix issue about imports componet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ConsultoresComponent } from './component/consultores/consultores.component';
 import { MatMenuModule } from '@angular/material/menu';
-import { MenuComponent } from './component/menu/menu.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
-import { HomeComponent } from './component/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // Importa los módulos de formularios
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
-import { ClienteTableComponent } from './component/cliente-table/cliente-table.component';
 import { MatTableModule } from '@angular/material/table';
-import { DesempehnoConsultorComponent } from './component/desempehno-consultor/desempehno-consultor.component';
-import { GraficoBarDialogComponent } from './component/grafico-bar-dialog/grafico-bar-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
