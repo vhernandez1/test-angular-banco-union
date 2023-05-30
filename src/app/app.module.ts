8680d9014b7e6ac5fcf0b793d034ec551d71e880
vhernandez1 <ing.victorhhernandez@gmail.com>
2023-05-30 15:17:53 -0500

    add imports angular components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ConsultoresComponent } from './component/consultores/consultores.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MenuComponent } from './component/menu/menu.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { HomeComponent } from './component/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // Importa los módulos de formularios
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { ClienteTableComponent } from './component/cliente-table/cliente-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { DesempehnoConsultorComponent } from './component/desempehno-consultor/desempehno-consultor.component';
+import { GraficoBarDialogComponent } from './component/grafico-bar-dialog/grafico-bar-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
@@ -12,8 +30,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatTableModule,
+    MatDialogModule],
   providers: [],
   bootstrap: [AppComponent]
 })
